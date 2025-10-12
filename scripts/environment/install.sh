f6ef260dd6a6929d61fcad20c3e0ed95b9d5153a
RyanOatmeal <ryanoatmeal94@outlook.com>
2025-10-12 14:21:58 -0500

    Trying to get the new containerfile to use a custom branch.

--- scripts/environment/install.sh
+++ scripts/environment/install.sh
@@ -8,7 +8,10 @@ echo "Copied in secrets.env file."
 # podman at the end of this command is the name of the folder podman at the 
 # root of the repository.
 echo "Beginning podman build:"
-podman build --tag $RYAN_OATMEAL_IMAGE_NAME podman
+
+BRANCH_NAME=${1:-main}
+podman build --build-arg BRANCH_NAME="${BRANCH_NAME}" --tag $RYAN_OATMEAL_IMAGE_NAME podman
+
 echo "Podman build succeeded."
 
 echo "Creating Container: $RYAN_OATMEAL_CONTAINER_NAME"
