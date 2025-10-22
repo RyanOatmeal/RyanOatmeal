4377fe51fafe6bbcccc93ea0e01f5b9a968a832b
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-10-21 20:30:39 -0500

    Adding an installation note and covering my ass.

--- scripts/nixos/deploy.sh
+++ scripts/nixos/deploy.sh
@@ -3,3 +3,8 @@
 sudo rm -r /etc/nixos
 sudo cp -r nixos /etc/nixos
 sudo nixos-rebuild switch
+# I have no idea how the auto genereation of this 
+# file works. So I'm just gonna move it into git 
+# everytime I deploy, idk if it'll ever change.
+rm nixos/config/hardware-configuration.nix
+cp /etc/nixos/config/hardware-configuration.nix nixos/config/hardware-configuration.nix
