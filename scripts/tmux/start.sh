1f6bcec64c66594af8304727702409172b60f200
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-11-26 08:28:33 -0600

    Fiddling with tmux

--- scripts/tmux/start.sh
+++ scripts/tmux/start.sh
@@ -6,7 +6,19 @@
 # Once that logic is setup, have `mod + enter` start your window
 # with a tmux session running.
 
+# Another todo is to get this script automatically splitting vertically. 
+
+# ALSO, we need an option where it fits to the terminal if we're in an 
+# unfamiliar situation. All through keybinds ofc.
+
 SESSION_NAME="ryan_oats"
 
-tmux new-session -d -s "$SESSION_NAME"
+TERMINAL_HEIGHT=$(tput lines)
+
+
+# I like to start with two windows vertically split with 80 characters each
+# of width. 
+# So including the dividing line, that's 161 characters.
+tmux new-session -d -s "$SESSION_NAME" -x 161 -y "$TERMINAL_HEIGHT"
+tmux set-option -t "$SESSION_NAME" window-size manual
 tmux attach-session -t "$SESSION_NAME"
