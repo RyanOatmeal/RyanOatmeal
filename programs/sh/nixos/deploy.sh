976b8f64dd25e7a0207e40b34b89b6d71962b459
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-11-29 08:35:27 -0600

    Moving nixos directory into programs.

--- programs/sh/nixos/deploy.sh
+++ programs/sh/nixos/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/dash
 
 sudo rm -r /etc/nixos
-sudo cp -r nixos /etc/nixos
+sudo cp -r programs/nixos /etc/nixos
 sudo nix-channel --update
 sudo nixos-rebuild switch
 sudo nix-collect-garbage -d
 # I have no idea how the auto generation of this 
 # file works. So I'm just gonna move it into git 
 # everytime I deploy, idk if it'll ever change.
-sudo rm nixos/config/hardware-configuration.nix
-sudo cp /etc/nixos/config/hardware-configuration.nix nixos/config/hardware-configuration.nix
+sudo rm programs/nixos/config/hardware-configuration.nix
+sudo cp /etc/nixos/config/hardware-configuration.nix programs/nixos/config/hardware-configuration.nix
--- programs/sh/nixos/test.sh
+++ programs/sh/nixos/test.sh
@@ -4,6 +4,6 @@
 # TODO: Place this in deploy. Just let it fail there.
 
 sudo rm -r /etc/nixos
-sudo cp -r nixos /etc/nixos
+sudo cp -r programs/nixos /etc/nixos
 sudo nix-channel --update
 sudo nixos-rebuild test
