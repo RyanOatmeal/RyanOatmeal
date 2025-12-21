2089e835f741787d57f69d016d2106813f7d5a0b
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-12-21 08:58:27 -0600

    Souping tmux for another repo.

---
+++ programs/sh/tmux/end.sh
@@ -0,0 +1,4 @@
+#!/bin/dash
+
+rm -r session_programs
+exec tmux kill-session
--- programs/sh/tmux/start.sh
+++ programs/sh/tmux/start.sh
@@ -1,6 +1,23 @@
 #!/bin/dash
 
+STARTING_DIR=$(pwd)
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 SESSION_NAME="ryan_oats"
+
+DIR="session_programs"
+
+if [ -e "$DIR" ]; then
+  printf "%s%s\n" \
+         "ERROR: $DIR exists. Delete it with " \
+         "\`rm -r session_programs\`"
+  exit 1
+else
+  cd $SCRIPT_DIR
+  cd ../../../
+  cp -r programs $STARTING_DIR/session_programs
+  cd $STARTING_DIR
+fi
 
 tmux new-session -d -s "$SESSION_NAME"
 tmux attach-session -t "$SESSION_NAME"
+
