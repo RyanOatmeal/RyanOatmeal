269420958476e9a8e9e692d0e87c348ee929e0db
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-12-25 10:08:07 -0600

    Removing test file and adding clean script.

---
+++ programs/sh/blog/clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/dash
+
+# This is the script that generates my static website. 
+# This must be ran in the root of the ryanoatmeal.github.io repo.
+
+make -C session_programs/sh/blog clean
--- programs/sh/blog/test.m4
+++
@@ -1,3 +0,0 @@
-dnl test m4 file, this is a comment.
-define(`TEST', `result')
-TEST
