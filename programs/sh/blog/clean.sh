12f6bd2fabdd5552285b071474c6639b8a0c9c01
ryanoatmeal <ryanoatmeal94@outlook.com>
2025-12-26 12:11:19 -0600

    Project planning and moving makefile over to website.

--- programs/sh/blog/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/dash
-
-# This is the script that will clean up the "target" directory
-# of the make file.
-
-# This must be ran in the root of the ryanoatmeal.github.io repo.
-
-make -C session_programs/sh/blog clean
--- programs/sh/blog/make.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/dash
-
-# This is the script that generates my static website. 
-# This must be ran in the root of the ryanoatmeal.github.io repo.
-
-make -C session_programs/sh/blog
