822ac54a4ab18716f9acd4c5d9db19ffa2ffa35b
ryanoatmeal <ryanoatmeal94@outlook.com>
2026-01-23 12:04:39 -0600

    Trying a fix

--- programs/sh/quickemu/bootstrap.sh
+++ programs/sh/quickemu/bootstrap.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/dash
 
 quickget windows 11
+sed -i "s|^fixed_iso|#fixed_iso|" windows-11.conf
--- programs/sh/quickemu/start.sh
+++ programs/sh/quickemu/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/dash
 
-quickemu --vm windows-11.conf --public-dir "./shared"
+quickemu --vm windows-11.conf --public-dir "./shared" --extra_args "--drive media=cdrom,index=3,file=windows-11/virtio-win.iso" 
