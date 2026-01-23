810833d0389078e71af038c05725c98521b1ed0b
ryanoatmeal <ryanoatmeal94@outlook.com>
2026-01-23 13:13:48 -0600

    Think I got quickemu working for windows 11. Fuck you windows for making it so hard to download and get the ISO. I had to go on an actual windows computer to build it with your media tool.

---
+++ programs/sh/quickemu/README.md
@@ -0,0 +1,5 @@
+Do the following when there's not a place to install:
+1. Expand the cd drive for virtio
+2. navigate to viostor -> w11 -> amd64
+3. Click ok
+4. Select the red hat controller
--- programs/sh/quickemu/bootstrap.sh
+++ programs/sh/quickemu/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/dash
 
 quickget windows 11
-sed -i "s|^fixed_iso|#fixed_iso|" windows-11.conf
+sed -i "s|^fixed_iso|# fixed_iso|" windows-11.conf
+cp ~/Binaries/Windows-11.iso windows-11/windows-11.iso
